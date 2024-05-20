5043e226eca1ccbf2dffdc003b39918f815227ce
Felipe <felipe_oliveira2019@hotmail.com>
2024-05-19 21:40:39 -0300

    adicionando conteudo dinamico no modal

--- js/code.js
+++ js/code.js
@@ -1,22 +1,34 @@
 class open {
-    constructor(cards, exit, modal) {
+    constructor(cards, exit, modal, title, contentModal) {
         this.cards = document.querySelectorAll(cards)
         this.exit = document.querySelector(exit)
         this.modal = document.querySelector(modal)
+        this.title = document.querySelector(title)
+        this.contentModal = document.querySelector(contentModal)
+    }
+
+    async insert(id) {
+        const response = await fetch("../json/pesquisas.json")
+        const pesquisas = await response.json()
+        this.title.innerText = pesquisas.dados[id].title
+        this.contentModal.innerText = pesquisas.dados[id].text
     }
 
     OpenModal = () => {
         this.cards.forEach((element, index) => {
             element.addEventListener('click', () => {
                 this.modal.classList.add("active")
+                this.insert(index)
             })
         })
     }
+
     closeModal = () => {
         this.exit.addEventListener('click', () => {
             this.modal.classList.remove("active")
         })
     }
+
 
     init = () => {
         this.closeModal()
@@ -25,5 +37,5 @@ class open {
     }
 }
 
-const OpenCards = new open('.card_theme', ".close_modal", ".modal")
+const OpenCards = new open('.card_theme', ".close_modal", ".modal", ".content_title_modal", ".content_text_modal")
 OpenCards.init()
